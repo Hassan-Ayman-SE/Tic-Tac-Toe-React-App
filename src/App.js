39cdaaca31cbd0bd03b9c3e2836570091f516950
Hassan-Ayman-SE <hsankraz66@gmail.com>
2024-05-01 15:15:17 +0000

    UPD code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Square from "./components/Square";
 import { useState } from "react";
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
 
-  //squares = ["null", "null", "null", ....9]
-  //nextSquares = ["X", "X", "null", ...9] copy squares
-  //immutability ex undo
+function Board({ xIsNext, squares, onPlay }) {
+  function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    onPlay(nextSquares);
+  }
 
   const winner = calculateWinner(squares);
   let status;
@@ -15,44 +22,68 @@ export default function Board() {
   } else {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
-  function handleClick(i) {
-    if (squares[i] || calculateWinner(squares)) {
-      return;
-    }
-
-    const nextSquares = squares.slice(); //copy squares arr
-    if (xIsNext) {
-      nextSquares[i] = "X";
-    } else {
-      nextSquares[i] = "O";
-    }
-
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
-
-    //Or you can use this syntax
-    // const newSquares = [...squares]; to copy arr
-  }
 
   return (
     <>
       <div className="status">{status}</div>
       <div className="board-row">
-        <Square onSquareClick={() => handleClick(0)} value={squares[0]} />
-        <Square onSquareClick={() => handleClick(1)} value={squares[1]} />
-        <Square onSquareClick={() => handleClick(2)} value={squares[2]} />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square onSquareClick={() => handleClick(3)} value={squares[3]} />
-        <Square onSquareClick={() => handleClick(4)} value={squares[4]} />
-        <Square onSquareClick={() => handleClick(5)} value={squares[5]} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square onSquareClick={() => handleClick(6)} value={squares[6]} />
-        <Square onSquareClick={() => handleClick(7)} value={squares[7]} />
-        <Square onSquareClick={() => handleClick(8)} value={squares[8]} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
+  );
+}
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
   );
 }
 
